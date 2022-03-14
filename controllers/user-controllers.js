11bcfafd3b685e0091737eba45a5484d1b18a0d4
elijahb22 <elibusick11@gmail.com>
2022-03-14 03:01:34 -0400

    added neccessary code to run server and created delete user method

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -41,6 +41,17 @@ const userContr =  {
             res.sendStatus(400);
         });
     },
+    deleteUser ({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err))
+    },
 }
 
 module.exports = userContr;
