a3dab396a5dac2123abf873020450c51ff6ab4ea
elijahb22 <elibusick11@gmail.com>
2022-03-14 04:25:29 -0400

    created add and delete friend method

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,6 +1,6 @@
 const { User, Thought } = require('../models/index')
 
-const userContr =  {
+const userController =  {
     getAllUsers(req,res) {
         User.find({})
         .populate({
@@ -63,6 +63,30 @@ const userContr =  {
         })
         .catch(err => res.status(400).json(err));
     },
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId},
+            { $addToSet: { friends: params.friendId }},
+            { new: true }
+        )
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this user id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: {friends: params.friendId} },
+            {new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    }
 }
 
-module.exports = userContr;
+module.exports = userController;
