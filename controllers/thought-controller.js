9c1f18bd91f89fb5b2ab3f60b6886987656e9620
elijahb22 <elibusick11@gmail.com>
2022-03-14 12:20:09 -0400

    created methods for updating thoughts, add reactions, and delete reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -59,6 +59,41 @@ const thoughtController = {
         })
         .catch(err => res.status(400).json(err))
     },
+    updateThought ({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id}, body, {new: true })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(400).json({ message: 'No thoughts found with this id!'});
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    addReaction ({ params, body }, res) {
+         Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: body } },
+            { new: true}
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(400).json({ message: "No thought found with this id number"});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(500).json(err))
+    },
+    deleteReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: {reactions: params.reactionId} },
+            {new: true }
+        )
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
+    }
 }
 
 module.exports = thoughtController;
