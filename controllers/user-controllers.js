d4d493f8ff4e42d9e1d040bc0991908da6f86192
elijahb22 <elibusick11@gmail.com>
2022-03-14 02:06:13 -0400

    created get user by id path

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -14,6 +14,25 @@ const userContr =  {
             res.sendStatus(400);
         });
     },
+     getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+           .populate({
+               path: 'thoughts',
+               select: '-__v'
+            })
+            .populate ({
+                path: 'friends',
+                select: '-__v'
+            })
+           .select('-__v')
+           .then(dbUserData => res.json(dbUserData))
+           .catch(err => {
+               console.log(err)
+               res.status(400).json(err)
+            
+        });
+        
+    },
     createUser({ body }, res) {
         User.create(body)
         .then(dbUserData => res.sendStatus(200))
